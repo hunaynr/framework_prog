99a6fdacfaeab745cb3f791172da65008ab1f854
hunaynr <hunaynr@gmail.com>
2021-02-22 17:27:26 +0700

    stateless_stateful_component

---
+++ 3rdWeek/hello_world/hello-world-app/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import HelloComponent from "./component/HelloComponent";
+class App extends Component {
+  render() {
+    return HelloComponent();
+  }
+}
+export default App;
