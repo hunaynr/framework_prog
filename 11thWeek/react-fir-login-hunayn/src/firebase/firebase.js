1e0527b6a19ac6d1023ba3909b14bb106cba34d3
hunaynr <hunaynr@gmail.com>
2021-04-24 15:23:11 +0700

    firebase login -first

---
+++ 11thWeek/react-fir-login-hunayn/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIzw2L6_fC6V3pg_FV_56RyZbjLHG6vhk",
+  authDomain: "fir-login-hunayn.firebaseapp.com",
+  databaseURL: "https://fir-login-hunayn-default-rtdb.firebaseio.com",
+  projectId: "fir-login-hunayn",
+  storageBucket: "fir-login-hunayn.appspot.com",
+  messagingSenderId: "189410526567",
+  appId: "1:189410526567:web:f68f97d0d41bbe0999dbc2",
+  measurementId: "G-FL6MGT379N",
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
